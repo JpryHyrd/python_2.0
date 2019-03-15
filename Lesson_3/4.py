b5f9974ef03b305434daa9cb9623ae68a483df87
JpryHyrd <42839731+JpryHyrd@users.noreply.github.com>
2019-03-15 23:23:08 +0300

    Lesson_3

--- Lesson_3/4.py
+++ Lesson_3/4.py
@@ -1,1 +1,20 @@
 # 4.	Определить, какое число в массиве встречается чаще всего.
+import random
+a = []
+g = 50
+for i in range(g):
+    a.append(random.randrange(10))
+print(a)
+b = 0
+c = 0
+r = 0
+for i in range(10):
+    for f in a:
+        if i == f:
+            b += 1
+    if b > r:
+        r = b
+        c = i
+    b = 0
+print("Самое часто встерчаемое число в данном массиве -", c)
+
