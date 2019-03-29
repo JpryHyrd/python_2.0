c6a1cb19d12b27011437888d6f1ad999ca5c7e50
JpryHyrd <42839731+JpryHyrd@users.noreply.github.com>
2019-03-29 20:20:01 +0300

    Lesson_7

--- Lesson_7/1.py
+++ Lesson_7/1.py
@@ -4,3 +4,28 @@
 исходный и отсортированный массивы. Сортировка должна быть реализована в
 виде функции. По возможности доработайте алгоритм (сделайте его умнее).
 """
+import random
+
+def bubble_sort(orig_list):
+    n = 1
+    while n < len(orig_list):
+        for i in range(len(orig_list)-n):
+            if orig_list[i] < orig_list[i+1]:
+                orig_list[i],orig_list[i+1] = orig_list[i+1],orig_list[i]
+        n += 1
+    return orig_list
+
+orig_list = [random.randint(-100, 100) for _ in range(20)]
+print("Исходный массив: ", end = "")
+print(orig_list)
+print("Массив после сортировки: ", end = "")
+print(bubble_sort(orig_list))
+
+
+
+
+
+
+
+
+
--- Lesson_7/3.py
+++ Lesson_7/3.py
@@ -6,3 +6,62 @@
 массива. Но если это слишком сложно, то используйте метод сортировки,
  который не рассматривался на уроках
 """
+print()
+import random
+def generation():
+    m = random.randint(20, 100)
+    b = (2 * m) + 1
+    lst = []
+    for i in range(b):
+        a = random.randint(1, 100)
+        if a not in lst:
+            lst.append(a)
+    if len(lst) % 2 == 0:
+        del(lst[0])
+    return lst
+
+
+def medium(lst):
+    print("Исходный массив: ")
+    print(lst)
+    a = int(len(lst) / 2 - 0.5)
+    t = 0
+    for i in range(len(lst)):
+        c = 0
+        ls1 = []
+        ls2 = []
+        for j in lst:
+            if i > j:
+                c += 1
+                ls1.append(j)
+            elif i < j:
+                ls2.append(j)
+        if c == a:
+            t = i
+            big = ls2
+            small = ls1
+
+#        Traceback (most recent call last):
+#        File "3.py", line 58, in <module>
+#        medium(generation())
+#        File "3.py", line 51, in medium
+#        print(small)
+#        UnboundLocalError: local variable 'small' referenced before assignment
+
+#Если не вставить это условие, то иногда выскакивает ошибка, описанная выше 
+    if t == 0: 
+        medium(generation())
+        return " "
+
+    print()
+    print("Медиана массива - " + str(t))
+    print()
+    print("Числа, меньше медианы: ")
+    print(small)
+    print()
+    print("Числа, больше медианы: ")
+    print(big)            
+#    print(len(big), len(small), len(lst))
+
+
+medium(generation())
