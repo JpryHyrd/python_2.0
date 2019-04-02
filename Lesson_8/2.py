db0a24e0fe653b7360d1c994fe91cfdd8babb86b
JpryHyrd <42839731+JpryHyrd@users.noreply.github.com>
2019-04-02 16:02:01 +0300

    Lesson_8

--- Lesson_8/2.py
+++ Lesson_8/2.py
@@ -3,3 +3,28 @@
 Пусть дана строка S длиной N, состоящая только из маленьких латинских букв.
 Требуется найти количество различных подстрок в этой строке.
 """
+import hashlib
+import random
+
+n = random.randrange(100)
+alpha = "abcdefghijklmnopqrstuvwxyz "
+s = ""
+for i in range(n):
+    g = random.randrange(27)
+    s += alpha[g]
+
+r = set()
+
+N = len(s)
+for i in range(N):
+    if i == 0:
+        N = len(s) - 1
+    else:
+        N = len(s)
+    for j in range(N, i, -1):
+        r.add(hashlib.sha1(s[i:j].encode('utf-8')).hexdigest())
+print("Количество различных подстрок в строке '%s' равно %d" % (s, len(r)))
+
+
+
+
